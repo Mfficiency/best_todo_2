07aea8466ae095807615e5a2fdb146cfceec72dc
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2025-08-15 12:13:05 +0200

    Add Android widget displaying app version

---
+++ android/app/src/main/kotlin/com/example/best_todo_2/VersionWidgetProvider.kt
@@ -0,0 +1,20 @@
+package com.example.best_todo_2
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+class VersionWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.version_widget)
+            views.setTextViewText(R.id.appwidget_text, BuildConfig.VERSION_NAME)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
