4b900670502d5b05d52d4824199c52825dd8d3d8
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2025-08-17 06:53:34 +0200

    Ensure white background with black text

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,13 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Best Todo 2',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: ThemeData.light()
+            .textTheme
+            .apply(bodyColor: Colors.black, displayColor: Colors.black),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: Config.darkMode ? ThemeMode.dark : ThemeMode.light,
       home: const HomePage(),
