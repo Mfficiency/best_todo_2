d21b920859390bc8b16d8accce715a9d6157d19f
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2025-08-17 13:25:11 +0200

    Style Android widget with green background and red text

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Best Todo 2',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.green,
-        textTheme: ThemeData.light()
-            .textTheme
-            .apply(bodyColor: Colors.red, displayColor: Colors.red),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData.dark(),
       themeMode: Config.darkMode ? ThemeMode.dark : ThemeMode.light,
       home: const HomePage(),
