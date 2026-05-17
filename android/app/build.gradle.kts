04dcc4190f6b192b4d7bd7ce87336b91e192238b
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2026-05-17 10:07:54 +0200

    solving previous thing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ val hasReleaseKeystore = listOf("keyAlias", "keyPassword", "storeFile", "storePa
 android {
     namespace = "com.mfficiency.best_todo_2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
